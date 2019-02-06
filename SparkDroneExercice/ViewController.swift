e32f7ea05d659b08408a53a11d41e7db403827af
Vincent Riva <vincent.riva@outlook.fr>
2019-02-06 04:31:34 +0100

    Initial Commit

---
+++ SparkDroneExercice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SparkDroneExercice
+//
+//  Created by Vincent Riva on 06/02/2019.
+//  Copyright © 2019 Vincent Riva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
