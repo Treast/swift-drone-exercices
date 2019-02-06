c3ad544069d032bd69196ddbaacf67f2e2bc23b3
Vincent Riva <treast@macbook-pro-de-vincent.home>
2019-02-06 06:12:24 +0100

    Replace views with UIImageViews

--- SparkDroneExercice/Exercice1ViewController.swift
+++ SparkDroneExercice/Exercice1ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import DJISDK
+import VideoPreviewer
 
 class Exercice1ViewController: UIViewController {
 
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var snapshotView1: UIImageView!
+    @IBOutlet weak var snapshotView2: UIImageView!
+    
+    var previewer = VideoPreviewer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let spark = DJISDKManager.product() as? DJIAircraft {
+            spark.camera?.delegate = self
+            spark.gimbal?.delegate = self
+            setupPreviewVideo()
+        }
+    }
+    
+    func setupPreviewVideo() {
+        previewer?.setView(cameraView)
+        if let _ = DJISDKManager.product(){
+            DJISDKManager.videoFeeder()?.primaryVideoFeed.add(self, with: nil)
+        }
+        previewer?.start()
     }
 
 
+    
+    func takeScreenshot() {
+        self.previewer?.snapshotThumnnail { (image) in
+            if let img = image {
+                if let snapshot1 = self.snapshotView1.image {
+                    self.snapshotView2.image = img
+                } else {
+                    self.snapshotView1.image = img
+                }
+            }
+        }
+    }
+}
+
+extension Exercice1ViewController: DJIVideoFeedListener {
+    func videoFeed(_ videoFeed: DJIVideoFeed, didUpdateVideoData videoData: Data) {
+        videoData.withUnsafeBytes { (bytes:UnsafePointer<UInt8>) in
+            previewer?.push(UnsafeMutablePointer(mutating: bytes), length: Int32(videoData.count))
+        }
+    }
+}
+
+extension Exercice1ViewController: DJICameraDelegate {
+    
+}
+
+extension Exercice1ViewController: DJIGimbalDelegate {
+    
 }
 
